dbefcc5a34c63dabbbe54fd891a35d19440aa493
mhdmardini <47790823+mhdmardini@users.noreply.github.com>
2019-02-19 14:07:11 -0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,13 +8,14 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Access the Gurus</h1>
-                <p>Fed up with agencies that over-promise and under-deliver? Gteat news, We are not an agency! We work directly with your leadership team to establish objectives, and we guarantee results!</p>
+                <h1>Dimension</h1>
+                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
+                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>products</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
