586747c8410ece90ab10d7520fbe160fde6c845d
mhdmardini <47790823+mhdmardini@users.noreply.github.com>
2019-02-23 03:23:07 -0800

    css

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,12 +10,12 @@ const Header = (props) => (
         <div className="content">
             <div className="inner">
                 <h1>Access the Gurus</h1>
-                <p>Fed up with agencies that over-promise and under-deliver? Gteat news, We are not an agency! We work directly with your leadership team to establish objectives, and we guarantee results!</p>
+                <p>Fed up with agencies that over-promise and under-deliver? Great news, We are not an agency! We work directly with your leadership team to establish objectives, and we guarantee results!</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>products</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>product</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
