0ef195e8f5f924884395c08cbd30893cb5c9f8e8
mhdmardini <47790823+mhdmardini@users.noreply.github.com>
2019-02-24 12:26:21 -0800

    contact link

--- src/components/Main.js
+++ src/components/Main.js
@@ -83,11 +83,11 @@ class Main extends React.Component {
             </ul>
           </form>
           <ul className="icons">
-            <li><a href="https://www.linkedin.com/company/gurus-ae/" className="icon fa-linkedin"><span className="label">Linkedin</span></a></li>
-            <li><a href="https://www.facebook.com/ae.gurus" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="https://www.instagram.com/gurus.ae/" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="https://www.twitter.com/Gurus_ae" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="https://github.com/gurus-digital" className="icon fa-github"><span className="label">GitHub</span></a></li>
+            <li><a href="https://www.linkedin.com/company/gurus-ae/" className="icon fa-linkedin" target="_blank"><span className="label">Linkedin</span></a></li>
+            <li><a href="https://www.facebook.com/ae.gurus" className="icon fa-facebook" target="_blank"><span className="label">Facebook</span></a></li>
+            <li><a href="https://www.instagram.com/gurus.ae/" className="icon fa-instagram" target="_blank"><span className="label">Instagram</span></a></li>
+            <li><a href="https://www.twitter.com/Gurus_ae" className="icon fa-twitter" target="_blank"><span className="label">Twitter</span></a></li>
+            // <li><a href="https://github.com/gurus-digital" className="icon fa-github" target="_blank"><span className="label">GitHub</span></a></li>
 
           </ul>
           {close}
